c72b588d83a6c0fb8c058916eecdfc229661668b
adimpdl <ghag@mpdl.mpg.de>
2022-05-11 08:57:08 +0200

    changes

--- server/controllers/validatorDetailsController.js
+++ server/controllers/validatorDetailsController.js
@@ -4,7 +4,7 @@ exports.getValidators = async (req, res) => {
   NodeValidator.find()
     .then((nodes) => {
       if (nodes) res.status(200).json(nodes)
-      else res.status(404).send('No nodes found')
+      else res.status(200).send('database building')
     })
     .catch((err) => {
       console.log('errerrerr', err)
